df24759505a22493cb827500f00f003771f7aeae
AoMedium <anoddmedium@gmail.com>
2025-02-21 11:35:49 +1300

    add DeckGL component with interactivity

--- MemoryAlbumClient/src/components/layouts/deckgl-test.tsx
+++ MemoryAlbumClient/src/components/layouts/deckgl-test.tsx
@@ -1,17 +1,28 @@
 import Map from "react-map-gl/maplibre";
 import "maplibre-gl/dist/maplibre-gl.css";
 import { BASEMAP } from "@deck.gl/carto";
+import DeckGL from "deck.gl";
+import { MapView } from "@deck.gl/core";
 
 export default function DeckGLTest() {
   return (
-    <Map
+    <DeckGL
+      // Add initial view state and controller here instead of in Map component for interactivity
       initialViewState={{
         longitude: -100,
         latitude: 40,
         zoom: 3.5,
       }}
-      style={{ width: "100vw", height: "100vh" }}
-      mapStyle={BASEMAP.VOYAGER}
-    />
+      controller
+    >
+      {/* See https://github.com/visgl/deck.gl/issues/7304#issuecomment-1277850750) */}
+      {/* @ts-expect-error: cannot be used as JSX component */}
+      <MapView>
+        <Map
+          style={{ width: "100vw", height: "100vh" }}
+          mapStyle={BASEMAP.VOYAGER}
+        />
+      </MapView>
+    </DeckGL>
   );
 }
