fb590b7d83f23511e1af350695805e83b21aa3ec
AoMedium <anoddmedium@gmail.com>
2025-02-21 19:42:27 +1300

    add mui

--- MemoryAlbumClient/src/app/index.tsx
+++ MemoryAlbumClient/src/app/index.tsx
@@ -1,5 +1,15 @@
 import MapLayout from "@/components/layouts/map-layout";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import MuiTest from "./routes/app/mui-test";
+
 export const App = () => {
-  return <MapLayout />;
+  return (
+    <>
+      <MuiTest />
+    </>
+  );
 };
