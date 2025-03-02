bcc2c0d0de6d2b873302dc52f74ef855a11d32a5
AoMedium <anoddmedium@gmail.com>
2025-03-02 23:04:16 +1300

    add list item text for sidebar

--- MemoryAlbumClient/src/features/sidebar/components/sidebar-item.tsx
+++ MemoryAlbumClient/src/features/sidebar/components/sidebar-item.tsx
@@ -1,5 +1,11 @@
-import { ListItem, ListItemButton, ListItemIcon } from "@mui/material";
-import { JSX } from "react";
+import {
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import { JSX, useState } from "react";
 
 interface SidebarItemProps {
   text?: string;
@@ -8,11 +14,15 @@ interface SidebarItemProps {
 }
 
 export default function SidebarItem(props: SidebarItemProps) {
+  const [isVisible, setVisible] = useState(false);
+
   return (
     <ListItem sx={{ justifyContent: "center" }} key={props.text} disablePadding>
       <ListItemButton
         sx={{ padding: "15px", borderRadius: "10px" }}
         onClick={props.onClick}
+        onMouseEnter={() => setVisible(true)}
+        onMouseLeave={() => setVisible(false)}
       >
         <ListItemIcon
           sx={{
@@ -22,7 +32,40 @@ export default function SidebarItem(props: SidebarItemProps) {
         >
           {props.icon}
         </ListItemIcon>
-        {/* <ListItemText primary={props.text} /> */}
+        {props.text && (
+          <ListItemText
+            sx={[
+              {
+                position: "absolute",
+                left: "110%",
+                background: "white",
+                borderRadius: "10px",
+                padding: "8px 20px",
+                boxShadow: "4px 4px 10px rgba(0,0,0,0.25)",
+
+                transition: "opacity 225ms, visibility 225ms",
+              },
+              isVisible && {
+                opacity: 1,
+                visibility: "visible",
+              },
+              !isVisible && {
+                opacity: 0,
+                visibility: "hidden",
+              },
+            ]}
+            disableTypography // Disable default nested Typography component
+          >
+            <Typography
+              sx={{
+                fontSize: "14px",
+                color: "grey",
+              }}
+            >
+              {props.text}
+            </Typography>
+          </ListItemText>
+        )}
       </ListItemButton>
     </ListItem>
   );
