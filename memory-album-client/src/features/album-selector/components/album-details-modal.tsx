ac8b774d0abfb01f027d5fc56264ee219fb232d9
AoMedium <anoddmedium@gmail.com>
2025-03-12 19:28:15 +1300

    add cover photo display panel

--- memory-album-client/src/features/album-selector/components/album-details-modal.tsx
+++ memory-album-client/src/features/album-selector/components/album-details-modal.tsx
@@ -5,6 +5,7 @@ import {
 } from '@/state/album/album-selection-slice';
 import { setAlbum } from '@/state/album/album-slice';
 import { RootState } from '@/state/store';
+import { ImageNotSupported } from '@mui/icons-material';
 import { Button, Stack, TextField, Typography } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -18,66 +19,82 @@ export default function AlbumDetailsModal() {
   return (
     <>
       {selectedAlbum ? (
-        <ModalContainer
+        <Stack
+          direction="row"
+          spacing={2}
           sx={{
-            display: 'flex',
             position: 'fixed',
             top: '50%',
             left: '50%',
             transform: 'translate(-50%, -50%)',
-
-            padding: 0, // Override as MUI text field label gets cut off
           }}
         >
-          <Stack
-            spacing={2}
+          <ModalContainer>
+            {selectedAlbum.coverPhotoId ? (
+              <>{/* TODO: display cover photo */}</>
+            ) : (
+              <>
+                <ImageNotSupported />
+                <Typography>No cover</Typography>
+              </>
+            )}
+          </ModalContainer>
+          <ModalContainer
             sx={{
-              overflow: 'scroll',
-              padding: 2,
+              display: 'flex',
+              padding: 0, // Override as MUI text field label gets cut off
             }}
           >
-            <Stack spacing={2}>
-              <TextField
-                label="Title"
-                disabled
-                value={selectedAlbum.title || 'No title'}
-              />
-              <TextField
-                label="Description"
-                multiline
-                maxRows={2}
-                disabled
-                value={selectedAlbum.description || 'No description'}
-              />
-            </Stack>
+            <Stack
+              spacing={2}
+              sx={{
+                overflow: 'scroll',
+                padding: 2,
+              }}
+            >
+              <Stack spacing={2}>
+                <TextField
+                  label="Title"
+                  disabled
+                  value={selectedAlbum.title || 'No title'}
+                />
+                <TextField
+                  label="Description"
+                  multiline
+                  maxRows={2}
+                  disabled
+                  value={selectedAlbum.description || 'No description'}
+                />
+              </Stack>
 
-            <Stack spacing={1}>
-              <Button
-                variant="contained"
-                onClick={() => {
-                  if (!selectedAlbum) {
-                    throw new Error(
-                      'currentAlbum should not be undefined at this point.',
-                    );
-                  }
-                  dispatch(setAlbum(selectedAlbum));
-                  dispatch(setDetailsModalOpen(false));
-                  dispatch(setSelectionModalOpen(false));
-                }}
-              >
-                Select
-              </Button>
-              <Button
-                variant="outlined"
-                onClick={() => {
-                  dispatch(setDetailsModalOpen(false));
-                }}
-              >
-                Cancel
-              </Button>
+              <Stack spacing={1}>
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    if (!selectedAlbum) {
+                      throw new Error(
+                        'currentAlbum should not be undefined at this point.',
+                      );
+                    }
+                    dispatch(setAlbum(selectedAlbum));
+                    dispatch(setDetailsModalOpen(false));
+                    dispatch(setSelectionModalOpen(false));
+                  }}
+                >
+                  Select
+                </Button>
+                <Button
+                  variant="outlined"
+                  onClick={() => {
+                    dispatch(setDetailsModalOpen(false));
+                  }}
+                >
+                  Cancel
+                </Button>
+              </Stack>
             </Stack>
-          </Stack>
-        </ModalContainer>
+          </ModalContainer>
+        </Stack>
       ) : (
         <Typography>Select an album</Typography>
       )}
