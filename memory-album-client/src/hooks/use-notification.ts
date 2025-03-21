9fbd1bda137f5d46a028d561387a7ca7b67304fa
AoMedium <anoddmedium@gmail.com>
2025-03-22 08:38:39 +1300

    add inform notification

--- memory-album-client/src/hooks/use-notification.ts
+++ memory-album-client/src/hooks/use-notification.ts
@@ -1,7 +1,10 @@
 import { SnackbarKey, useSnackbar } from 'notistack';
 
+const autoHideDuration = 2000;
+
 export default function useNotification(): {
   notify: (message: string) => SnackbarKey;
+  inform: (message: string) => SnackbarKey;
   reportSuccess: (message: string) => SnackbarKey;
   warn: (message: string) => SnackbarKey;
   throwError: (message: string) => SnackbarKey;
@@ -11,27 +14,33 @@ export default function useNotification(): {
 
   const notify = (message: string) =>
     enqueueSnackbar(message, {
-      autoHideDuration: 5000,
+      autoHideDuration,
       variant: 'default',
+    });
+
+  const inform = (message: string) =>
+    enqueueSnackbar(message, {
+      autoHideDuration,
+      variant: 'info',
     });
 
   const reportSuccess = (message: string) =>
     enqueueSnackbar(message, {
-      autoHideDuration: 5000,
+      autoHideDuration,
       variant: 'success',
     });
 
   const warn = (message: string) =>
     enqueueSnackbar(message, {
-      autoHideDuration: 5000,
+      autoHideDuration,
       variant: 'warning',
     });
 
   const throwError = (message: string) =>
     enqueueSnackbar(message, {
-      autoHideDuration: 5000,
+      autoHideDuration,
       variant: 'error',
     });
 
-  return { notify, reportSuccess, warn, throwError };
+  return { notify, inform, reportSuccess, warn, throwError };
 }
