fe3a175042d9cda0dd1b97cadbbd3006f822a3c3
AoMedium <anoddmedium@gmail.com>
2025-03-25 19:19:12 +1300

    Revert "move features into albums and events"
    This reverts commit 9f593842ae2ba9cfea43bed68dc60c97df5ca78f.

--- memory-album-client/src/components/layouts/map-layout.tsx
+++ memory-album-client/src/components/layouts/map-layout.tsx
@@ -1,9 +1,9 @@
 import 'maplibre-gl/dist/maplibre-gl.css';
 import Sidebar from '@/features/sidebar/components/sidebar';
-import AlbumSelector from '@/features/albums/components/album-selector';
-import EventCreationPanel from '@/features/events/components/event-creation-panel';
-import CreateEventButton from '@/features/events/components/create-event-button';
-import AlbumMap from '@/features/albums/components/album-map';
+import AlbumSelector from '@/features/album-selector/components/album-selector';
+import EventCreationPanel from '@/features/create-event/components/event-creation-panel';
+import CreateEventButton from '@/features/create-event/components/create-event-button';
+import AlbumMap from '@/features/album-map/components/album-map';
 
 export default function MapLayout() {
   return (
