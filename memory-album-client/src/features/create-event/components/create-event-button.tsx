286c74eaec37205b4f7d740fb972c8de9337d7c8
AoMedium <anoddmedium@gmail.com>
2025-04-06 13:56:51 +1200

    replace create event button with mui SpeedDial

--- memory-album-client/src/features/create-event/components/create-event-button.tsx
+++ memory-album-client/src/features/create-event/components/create-event-button.tsx
@@ -1,8 +1,15 @@
 import { styles } from '@/config/constants';
 import { setCreationPanelOpen } from '@/state/event/event-creation-slice';
 import { RootState } from '@/state/store';
-import { Add } from '@mui/icons-material';
-import { IconButton } from '@mui/material';
+import { Event, Room } from '@mui/icons-material';
+import {
+  Backdrop,
+  Box,
+  SpeedDial,
+  SpeedDialAction,
+  SpeedDialIcon,
+} from '@mui/material';
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 export default function CreateEventButton() {
@@ -10,34 +17,82 @@ export default function CreateEventButton() {
     (state: RootState) => state.eventCreation.isCreationPanelOpen,
   );
 
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   const dispatch = useDispatch();
 
   return (
-    <>
-      {!isCreationPanelOpen && (
-        <IconButton
-          sx={{
-            position: 'absolute',
-            bottom: `calc(2 * ${styles.viewport.margin})`,
-            right: `calc(2 * ${styles.viewport.margin})`,
+    <Box
+      sx={{
+        position: 'absolute',
+        bottom: `calc(1.5 * ${styles.viewport.margin})`,
+        right: `calc(1.5 * ${styles.viewport.margin})`,
+        height: 330,
 
-            background: (theme) => theme.palette.background.paper,
-            boxShadow: (theme) => theme.shadows[styles.boxShadow.height],
-
-            color: (theme) => theme.palette.text.primary,
-
-            ':hover': {
-              background: (theme) => theme.palette.primary.main,
-              color: (theme) => theme.palette.primary.contrastText,
-            },
-          }}
-          onClick={() => {
+        transform: 'translateZ(0px)',
+        flexGrow: 1,
+      }}
+    >
+      <Backdrop open={open} />
+      <SpeedDial
+        ariaLabel="Create element"
+        sx={{ position: 'absolute', bottom: 16, right: 16 }}
+        icon={<SpeedDialIcon />}
+        onClose={handleClose}
+        onOpen={handleOpen}
+        open={open}
+      >
+        {createAction({
+          icon: <Event />,
+          title: 'Event',
+          onClick: () => {
+            handleClose();
             dispatch(setCreationPanelOpen(true));
-          }}
-        >
-          <Add sx={{ fontSize: '35px' }} />
-        </IconButton>
-      )}
-    </>
+          },
+          disabled: isCreationPanelOpen,
+        })}
+        {createAction({
+          icon: <Room />,
+          title: 'Location',
+          onClick: () => {
+            handleClose();
+            dispatch(setCreationPanelOpen(true));
+          },
+          disabled: isCreationPanelOpen,
+        })}
+      </SpeedDial>
+    </Box>
+  );
+}
+
+interface ActionProps {
+  icon: React.ReactNode;
+  title: string;
+  onClick: () => void;
+  disabled?: boolean;
+}
+/**
+ * SpeedDial does not recognise SpeedDialActions if
+ * they are a child of a custom component.
+ *
+ * @returns SpeedDialAction element
+ */
+function createAction(props: ActionProps) {
+  return (
+    <SpeedDialAction
+      icon={props.icon}
+      tooltipTitle={props.title}
+      tooltipOpen
+      onClick={() => {
+        if (!props.disabled) {
+          props.onClick();
+        }
+      }}
+      sx={{
+        opacity: props.disabled ? 0.3 : 1,
+      }}
+    />
   );
 }
