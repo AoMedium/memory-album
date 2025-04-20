8400a346679d239ece2d8973d9d4dddd95296d44
AoMedium <anoddmedium@gmail.com>
2025-04-20 20:07:55 +1200

    add: page routing

--- memory-album-client/src/app/router.tsx
+++ memory-album-client/src/app/router.tsx
@@ -1,4 +1,18 @@
-/**
- * TODO:
- * - https://github.com/alan2207/bulletproof-react/blob/master/apps/react-vite/src/app/router.tsx
- */
+import { Navigate, useRoutes } from 'react-router';
+import { paths } from '@/config/paths';
+import MapLayout from '@/components/layouts/map-layout';
+
+export default function AppRouter() {
+  const routes = useRoutes([
+    {
+      path: paths.app.root.path,
+      element: <Navigate to={paths.app.map.path} replace />, // Redirect to /models
+    },
+    {
+      path: paths.app.map.path,
+      element: <MapLayout />,
+    },
+  ]);
+
+  return routes;
+}
--- memory-album-client/src/config/paths.tsx
+++ memory-album-client/src/config/paths.tsx
@@ -1,4 +1,33 @@
 /**
  * TODO:
  * - https://github.com/alan2207/bulletproof-react/blob/master/apps/react-vite/src/config/paths.ts
+ *
+ * "/" - map
+ *      "/:id" - album id
+ *      "/:id/events/:id" - event id
  */
+
+export const paths = {
+  app: {
+    root: {
+      path: '/',
+    },
+    map: {
+      path: '/map',
+      getHref: () => '/map',
+    },
+    album: {
+      path: '/map/:albumId',
+      getHref: (albumId: string) => `/map/${albumId}`,
+    },
+    events: {
+      path: '/map/:albumId/events',
+      getHref: (albumId: string) => `/map/${albumId}/events`,
+    },
+    event: {
+      path: '/map/:albumId/events/:eventId',
+      getHref: (albumId: string, eventId: string) =>
+        `/map/${albumId}/events/${eventId}`,
+    },
+  },
+};
