05e35eb879472cc2d37c1cd423cb90093bfffe33
AoMedium <anoddmedium@gmail.com>
2025-04-21 19:06:01 +1200

    group features into pages

--- memory-album-client/src/app/routes/app/map.tsx
+++ memory-album-client/src/app/routes/app/map.tsx
@@ -1,13 +1,13 @@
 import 'maplibre-gl/dist/maplibre-gl.css';
-import Sidebar from '@/features/sidebar/components/sidebar';
-import EventCreationPanel from '@/features/create-event/components/event-creation-panel';
-import AlbumMap from '@/features/album-map/components/album-map';
-import EventListModal from '@/features/event-list/components/event-list-modal';
-import CreateElementButton from '@/features/create-element/components/create-element-button';
+import Sidebar from '@/features/map/sidebar/components/sidebar';
+import EventCreationPanel from '@/features/map/create-event/components/event-creation-panel';
+import AlbumMap from '@/features/map/album-map/components/album-map';
+import EventListModal from '@/features/map/event-list/components/event-list-modal';
+import CreateElementButton from '@/features/map/create-element/components/create-element-button';
 import { Outlet } from 'react-router';
-import HeaderBar from '@/features/header-bar/components/header-bar';
-import AlbumSelectorModal from '@/features/album-selector/components/modal/album-selector-modal';
-import AlbumSelectorButton from '@/features/album-selector/components/album-selector-button';
+import HeaderBar from '@/features/map/header-bar/components/header-bar';
+import AlbumSelectorModal from '@/features/map/album-selector/components/modal/album-selector-modal';
+import AlbumSelectorButton from '@/features/map/album-selector/components/album-selector-button';
 
 export default function MapRoute() {
   return (
